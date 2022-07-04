a72820c793bde7915b1887c9ea961a3310d241cd
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2022-07-04 16:45:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+const comunidades =[
+    {
+        imgcomunidades: '/img/comunidade do pilar.jpg',
+        txtcomunidade: 'essa aqui falta tudo'
+    },
+
+    {
+        imgcomunidades: '/img/danilo-alvesd-RZlA_V9DhvE-unsplash.jpg',
+        txtcomunidade: 'Essa aqui agalou tudo'
+    },
+
+    {
+        imgcomunidades: '/img/comunidade2.jpg',
+        txtcomunidade: 'Essa aqui agalou tudo'
+    },
+
+    {
+        imgcomunidades: '/img/comunidade3.jpg',
+        txtcomunidade: 'essa comunidade ta so o pó'
+    }
+]
+
+let indexComunidades = 0
+let imagem = document.querySelector('.infos-content img')
+let titulo = document.querySelector('.infos-content h1')
+let txt = document.querySelector('.infos-content-txt p')
+
+document.querySelector('.voltar').addEventListener('click', ()=>{
+    indexComunidades--
+    slider(indexComunidades)
+})
+
+document.querySelector('.proximo').addEventListener('click', ()=>{
+    indexComunidades++
+    slider(indexComunidades)
+})
+
+function slider(index){
+    imagem.src = comunidades[index].imgcomunidades
+    txt.textContent = ('.infos-content-txt p', comunidades[index].txtcomunidade)
+
+}
+
+
+
 document.querySelector('.hamburguer').addEventListener('click', ()=>{
     document.querySelector('body').classList.toggle('showSidebar')
 })
