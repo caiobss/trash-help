1ac2bfb900d5c139243bfb8729d6166d446ad61b
Caio Bezerra <68752886+caiobss@users.noreply.github.com>
2022-07-07 16:42:42 -0300

    Add files via upload

--- second page/donate.js
+++ second page/donate.js
@@ -1,22 +1,22 @@
 const comunidades =[
     {
         imgcomunidades: '/img/comunidade do pilar.jpg',
-        txtcomunidade: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae quaerat, aut itaque sunt ullam maxime rem totam, eaque asperiores facere nihil cumque veritatis distinctio consectetur! Et voluptatibus nulla aperiam.'
+        txtcomunidade: 'Do alto de um corredor no primeiro andar, em um dos blocos, Maria Lúcia Gomes, 62 anos, aposentada, observa de máscara o movimento na rua e a entrada do seu prédio. “Eu tenho varizes e feridas nas pernas, aí não posso andar muito”, disse. Ela mora sozinha em um apartamento com um quarto, sala, cozinha e banheiro.'
     },
 
     {
         imgcomunidades: '/img/danilo-alvesd-RZlA_V9DhvE-unsplash.jpg',
-        txtcomunidade: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae quaerat, aut itaque sunt ullam maxime rem totam, eaque asperiores facere nihil cumque veritatis distinctio consectetur! Et voluptatibus nulla aperiam.'
+        txtcomunidade: 'Leandra Patrícia, 40, trabalha como ambulante e faz bicos como manicure. Ela destaca o trabalho das ONGs e empresas junto à comunidade. “O pessoal da Ponte entregou algumas cestas na semana passada. O Porto Digital também”, afirmou. Na última sexta-feira (27), a ONG Visão Mundial fez uma ação junto à Pilar distribuindo kits com material de prevenção contra a contaminação, como álcool em gel, desinfetante e álcool para limpeza. A organização também distribuiu cestas básicas.'
     },
 
     {
         imgcomunidades: '/img/comunidade2.jpg',
-        txtcomunidade: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae quaerat, aut itaque sunt ullam maxime rem totam, eaque asperiores facere nihil cumque veritatis distinctio consectetur! Et voluptatibus nulla aperiam.'
+        txtcomunidade: 'jairo Walterlan da Silva, 28, não tem emprego fixo. Ele conta que a falta de trabalho formal é um problema crônico da comunidade. “Boa parte das pessoas daqui está desempregada. O pessoal trabalha com comércio, vendendo bebida. Com essa situação toda, todo mundo está sem ter de onde tirar o sustento”, relatou. Segundo ele, seus vizinhos estão procurando se cuidar na medida do possível. “A gente busca lavar as mãos e zela para os mais velhos não irem muito à rua”, contou.'
     },
 
     {
         imgcomunidades: '/img/comunidade3.jpg',
-        txtcomunidade: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat repudiandae quaerat, aut itaque sunt ullam maxime rem totam, eaque asperiores facere nihil cumque veritatis distinctio consectetur! Et voluptatibus nulla aperiam.'
+        txtcomunidade: 'Rosineide Santana, 59, tem artrose e faz terapia no posto de saúde da localidade. Ela teve que parar o tratamento por conta da pandemia. “Não estou podendo mais ir desde que essa situação começou”, disse. Auxiliar de serviços gerais, está desempregada e recebe Bolsa Família. Mora com um filho adulto e dorme na sala de casa, juntando cadeiras numa cama improvisada.'
     }
 ]
 
